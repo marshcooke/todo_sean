ddb5ce1d5cf075eb872e946a79eae37ec6a53a9c
Sarah Cooke <marshcooke1@gmail.com>
2018-02-05 11:54:30 -0600

    POST route in todo.router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -29,4 +29,35 @@ router.get('/', function(req,res) {
     }
 });
 
+router.post('/', function (req, res) {
+    if (req.isAuthenticated()) {
+        console.log('user is logged in', req.user);
+        var userId = req.user.id;
+        var taskId = req.body[0].id;
+        console.log('in post / function, req.body: ', req.body);
+        pool.connect(function (connectionError, client, done) {
+            if (connectionError) {
+                console.log(connectionError);
+                res.sendStatus(500);
+            } else {
+                var queryString = 'INSTERT INTO users_tasks (users_id, tasks_id) VALUES ($1, $2) RETURNING tasks_id;';
+                var values = [userId, breweryId];
+                client.query(queryString, values, function (queryError, resultsObj) {
+                    done();
+                    if (queryError) {
+                        console.log(queryError);
+                        res.sendStatus(500);
+                    } else {
+                        console.log('resultsObj: ', resultsObj);
+                        res.sendStatus(201);
+                    }
+                });
+            }
+        });
+    } else {
+        console.log('not logged in');
+        res.send(false);
+    }
+});
+
 module.exports = router;
