2baa9f5317ce4d7cabde8df7bf7a7a2f4249b8cb
Sarah Cooke <marshcooke1@gmail.com>
2018-02-05 11:58:43 -0600

    sourcing todo router into server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var sessionConfig = require('./modules/session.config');
 var indexRouter = require('./routes/index.router');
 var userRouter = require('./routes/user.router');
 var registerRouter = require('./routes/register.router');
+var todoRouter = require('./routes/todo.router');
+
 
 var port = process.env.PORT || 5556;
 
@@ -31,6 +33,7 @@ app.use(passport.session());
 // Routes
 app.use('/register', registerRouter);
 app.use('/home', userRouter);
+app.use('/', todoRouter);
 
 // Catch all bucket, must be last!
 app.use('/', indexRouter);
