5f732f0da4403ba563f6524ab959cdfc474c0efc
Sarah Cooke <marshcooke1@gmail.com>
2018-02-26 14:59:03 -0600

    commenting out delete route in user.router for now

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -86,35 +86,35 @@ router.post('/', function (req, res) {
   }
 });
 
-router.delete('/:id', function (req, res) {
-  if (req.isAuthenticated()) {
-    console.log('user is logged in', req.user);
-    var userId = req.user.id;
-    var taskId = req.params.id;
-    console.log('in delete / function, req. params: ', req.params.id);
-    pool.connect(function (connectionError, client, done) {
-      if (connectionError) {
-        console.log(connectionError);
-        res.sendStatus(500);
-      } else {
-        var queryString = 'DELETE FROM users_tasks WHERE users_id = $1 AND tasks_id = $2;';
-        var values = [userId, tasksId];
-        client.query(queryString, values, function (queryError, resultsObj) {
-          done();
-          if (queryError) {
-            console.log(queryError);
-            res.sendStatus(500);
-          } else {
-            console.log('resultsObj: ', resultsObj);
-            res.sendStatus(201);
-          }
-        });
-      }
-    });
-  } else {
-    console.log('not logged in');
-    res.send(false);
-  }
-});
+// router.delete('/:id', function (req, res) {
+//   if (req.isAuthenticated()) {
+//     console.log('user is logged in', req.user);
+//     var userId = req.user.id;
+//     var taskId = req.params.id;
+//     console.log('in delete / function, req. params: ', req.params.id);
+//     pool.connect(function (connectionError, client, done) {
+//       if (connectionError) {
+//         console.log(connectionError);
+//         res.sendStatus(500);
+//       } else {
+//         var queryString = 'DELETE FROM users_tasks WHERE users_id = $1 AND tasks_id = $2;';
+//         var values = [userId, tasksId];
+//         client.query(queryString, values, function (queryError, resultsObj) {
+//           done();
+//           if (queryError) {
+//             console.log(queryError);
+//             res.sendStatus(500);
+//           } else {
+//             console.log('resultsObj: ', resultsObj);
+//             res.sendStatus(201);
+//           }
+//         });
+//       }
+//     });
+//   } else {
+//     console.log('not logged in');
+//     res.send(false);
+//   }
+// });
 
 module.exports = router;
