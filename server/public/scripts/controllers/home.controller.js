1a29f4d3195d706ead87be0ab60abd5550320442
Sarah Cooke <marshcooke1@gmail.com>
2018-03-02 13:02:03 -0600

    commenting out addtask function

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,15 +1,16 @@
 myApp.controller('HomeController', function (UserService) {
   console.log('HomeController created');
   var vm = this;
+
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
 
   vm.allTasksObj = UserService.allTasksObj;
 
-  vm.addTask = function (task) {
-    UserService.addTask(task);
-    console.log('add task hit in hc');
-  }
+  // vm.addTask = function (task) {
+  //   UserService.addTask(task);
+  //   console.log('add task hit in hc');
+  // }
 
   vm.getTasks = function () {
     UserService.getTasks();
