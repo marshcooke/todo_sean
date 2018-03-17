7176b944263b88e515e28399b4f8b81d7b9b23de
Sarah Cooke <marshcooke1@gmail.com>
2018-03-17 11:56:28 -0500

    changes in POST route

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -58,12 +58,12 @@ router.get('/home', function (req, res) {
   }
 });
 
-router.post('/home', function (req, res) {
+router.post('/', function (req, res) {
   if (req.isAuthenticated()) {
-    console.log('user is logged in', req.user);
+    console.log('user is logged in in router.post/home', req.user);
     var userId = req.user.id;
     var tasksId = req.body[0].id;
-    console.log('in post / function userId, tasksId: ', userId, tasksId);
+    console.log('in post function userId, tasksId: ', userId, tasksId);
     pool.connect(function (connectionError, client, done) {
       if (connectionError) {
         console.log(connectionError);
