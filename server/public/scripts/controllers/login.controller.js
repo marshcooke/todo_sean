4a0aff0037cd698037f3d39ceabe5feb869999da
Sarah Cooke <marshcooke1@gmail.com>
2018-03-27 06:24:34 -0500

    login controller formatting

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -1,48 +1,48 @@
-myApp.controller('LoginController', function($http, $location, UserService) {
-    console.log('LoginController created');
-    var vm = this;
+myApp.controller('LoginController', function ($http, $location, UserService) {
+  console.log('LoginController created');
+  var vm = this;
 
-    vm.user = {
-      username: '',
-      password: ''
-    };
-    vm.message = '';
+  vm.user = {
+    username: '',
+    password: ''
+  };
+  vm.message = '';
 
-    vm.login = function() {
-      console.log('LoginController -- login');
-      if(vm.user.username === '' || vm.user.password === '') {
-        vm.message = "Enter your username & password!";
-      } else {
-        console.log('LoginController -- login -- sending to server: ', vm.user.username);
-        $http.post('/', vm.user).then(function(response) {
-          if(response.data.username) {
-            console.log('LoginController -- login -- success: ', response.data.username);
-            // location works with SPA (ng-route)
-            $location.path('/home'); // http://localhost:5556/#/home
-          } else {
-            console.log('LoginController -- login -- failure: ', response);
-            vm.message = "Wrong!";
-          }
-        }).catch(function(response){
-          console.log('LoginController -- registerUser -- failure: ', response);
+  vm.login = function () {
+    console.log('LoginController -- login');
+    if (vm.user.username === '' || vm.user.password === '') {
+      vm.message = "Enter your username & password!";
+    } else {
+      console.log('LoginController -- login -- sending to server: ', vm.user.username);
+      $http.post('/', vm.user).then(function (response) {
+        if (response.data.username) {
+          console.log('LoginController -- login -- success: ', response.data.username);
+          // location works with SPA (ng-route)
+          $location.path('/home'); // http://localhost:5556/#/home
+        } else {
+          console.log('LoginController -- login -- failure: ', response);
           vm.message = "Wrong!";
-        });
-      }
-    };
-
-    vm.registerUser = function() {
-      console.log('LoginController -- registerUser');
-      if(vm.user.username === '' || vm.user.password === '') {
-        vm.message = "Choose a username and password!";
-      } else {
-        console.log('LoginController -- registerUser -- sending to server...', vm.user);
-        $http.post('/register', vm.user).then(function(response) {
-          console.log('LoginController -- registerUser -- success');
-          $location.path('/login');
-        }).catch(function(response) {
-          console.log('LoginController -- registerUser -- error');
-          vm.message = "Please try again."
-        });
-      }
+        }
+      }).catch(function (response) {
+        console.log('LoginController -- registerUser -- failure: ', response);
+        vm.message = "Wrong!";
+      });
     }
+  };
+
+  vm.registerUser = function () {
+    console.log('LoginController -- registerUser');
+    if (vm.user.username === '' || vm.user.password === '') {
+      vm.message = "Choose a username and password!";
+    } else {
+      console.log('LoginController -- registerUser -- sending to server...', vm.user);
+      $http.post('/register', vm.user).then(function (response) {
+        console.log('LoginController -- registerUser -- success');
+        $location.path('/login');
+      }).catch(function (response) {
+        console.log('LoginController -- registerUser -- error');
+        vm.message = "Please try again."
+      });
+    }
+  }
 });
